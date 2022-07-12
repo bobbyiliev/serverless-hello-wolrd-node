6820f6b2260bf81d856786c998d2e97894218409
Bobby Iliev <bobby@bobbyiliev.com>
2022-07-12 12:59:36 +0300

    Add postgres example

--- packages/sample/hello/hello.js
+++ packages/sample/hello/hello.js
@@ -1,15 +1,35 @@
+const fs = require('fs');
 const helloWorld = require('hello-world-npm')
+const { Client } = require('pg');
 
-function main(args) {
+async function main(args) {
+  // Base64 decode the process.env.CA_CERT
+  const caCert = Buffer.from(process.env.CA_CERT, 'base64').toString('utf8')
+  // store the cert in a file
+  fs.writeFileSync('ca.crt', caCert)
 
-    let name = args.name || 'stranger'
-    let greeting = 'Hello ' + name + '!'
-    console.log(greeting)
-
-    console.log(helloWorld());
-
-    return {"body": greeting + " " + helloWorld()}
+  const client = new Client({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    ssl: {
+      ca: caCert
+    }
+  });
+  // Instantiates a connection to the database and retrieves data from the `available-coffee` collection
+  try {
+    await client.connect();
+    const res = await client.query('SELECT * FROM users');
+    console.log(res.rows);
+    return {"body": res.rows}
+  } catch (err) {
+    console.log(err);
+  } finally {
+    await client.end();
   }
-  
-exports.main = main
 
+}
+
+exports.main = main
